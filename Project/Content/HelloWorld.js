6cc90d66af666e07e04dbe3dadcc00ec16d3b205
Mudrak <mpate487@my.centennialcollege.ca>
2016-05-10 13:04:51 -0500

    Add files via upload

---
+++ Project/Content/HelloWorld.js
@@ -0,0 +1,5 @@
+//Javascript goes here...
+Console.log("Program started...");
+document.write("Hello World!");
+Console.log("Program Ended");
+//copyright Mudrak Patel; Centennial College; Summer 2016; Clientside Web Design
